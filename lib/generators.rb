a36993cd2e5fe6cdd5f64c57ed7cc020e01a110c
zhandao <x@skippingcat.com>
2018-01-21 03:08:44 +0800

    runable

--- lib/generators.rb
+++ lib/generators.rb
@@ -1,4 +1,16 @@
-require "generators/version"
+require 'generators/version'
+
+files = %w[
+  helpers dsl
+  api_doc_support/config api_doc_support/dsl
+  jbuilder/config jbuilder/dsl
+  model_doc_support/config model_doc_support/helpers model_doc_support/dsl
+  rspec/config rspec/helpers rspec/dsl
+  rspec/model/helpers rspec/model
+  rspec/normal/helpers rspec/normal
+  rspec/request/helpers rspec/request
+].map { |path| path.prepend('generators/') }
+files.each { |file| require file }
 
 module Generators
   # Your code goes here...
