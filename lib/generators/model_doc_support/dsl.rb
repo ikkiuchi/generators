ab62ed440ded41cbc012c4648225c056e4c23d09
zhandao <x@skippingcat.com>
2019-03-17 20:26:28 +0800

    C / Remove `frozen_string_literal`

--- lib/generators/model_doc_support/dsl.rb
+++ lib/generators/model_doc_support/dsl.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 module Generators::ModelDocSupport
   module DSL
     def self.included(base)
--- lib/generators/model_doc_support/helpers.rb
+++ lib/generators/model_doc_support/helpers.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 module Generators::ModelDocSupport
   module Helpers
     include Generators::Helpers
